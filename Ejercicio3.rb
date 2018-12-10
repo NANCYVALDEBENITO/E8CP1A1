7d22d7e7e6127e128b9a1d1ba6273a43a74b3640
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-12-10 20:21:53 -0300

    add Ejercicio 3

--- Ejercicio3.rb
+++ Ejercicio3.rb
@@ -1,10 +1,23 @@
-def count_words(file)
+def count_words_1(file)
 	file = File.open(file,"r")
  	lines = file.readlines
  	words = []
  	lines.each{|i| words += i.chomp.split(' ') }
 	file.close
-	print words.count
+	puts words.count
 end
 
-count_words('peliculas.txt')
+count_words_1('peliculas.txt')
+
+def count_words_2(file,string)
+	file = File.open(file,"r")
+ 	lines = file.readlines
+ 	words = []
+ 	lines.each{|i| words += i.chomp.split(' ') }
+	file.close
+	counts = []
+	words.each{|i| counts.push(i) if i == string }
+	puts counts.count
+end
+
+count_words_2('peliculas.txt','La')
